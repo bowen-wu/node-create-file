6755cb2c4cebebf445cea3d41db6909b0dc62ce4
bowen-wu <13793248162@163.com>
2018-02-21 11:23:46 +0800

    添加 MVC 的模板 JS + eventHub.js

--- pc.js
+++ pc.js
@@ -20,6 +20,7 @@ if (fs.existsSync(dirName)) {
     fs.mkdirSync('src') // mkdir css
     fs.mkdirSync('src/css') // mkdir src/css
     fs.mkdirSync('src/js') // mkdir src/js
+    fs.mkdirSync('src/img') // mkdir src/img
     fs.mkdirSync('dist') // mkdir js
     
     
@@ -66,6 +67,83 @@ h1,h2,h3,h4,h5,h6{font-weight: normal;}
     fs.writeFileSync("./src/js/main.js", 
     `{
       
+}`)
+    fs.writeFileSync("./src/js/temp.js",
+    `{
+    let view = {
+        el: '',
+        template: '',
+        init() { // 初始化
+
+        },   
+        render(data = {}) { // 初始 render
+
+        }, 
+        create(data) { // 创建新的项
+            console.log(data)
+        }, 
+        update(data) { // 局部更新视图
+
+        }, 
+        // 切换状态函数
+        active() { // 激活，添加 active 类
+
+        },
+        deactive() { // 移除激活，移除 active 类
+
+        },
+    }
+    let model = {
+        data: {},
+        init() { // 初始化
+
+        }, 
+        create(data) { // 创建数据 ==> 更新 data
+            console.log(data)
+        },
+        update(data) { // 编辑数据 ==> 更新 data
+            console.log(data)
+        },
+    }
+    let controller = {
+        init(view, model) {
+            this.view = view
+            this.model = model
+            this.view.init()
+            this.model.init()
+            this.view.render(this.model.data)
+            this.bindEvents()
+            this.bindEventhub()
+        },
+        bindEvents(){
+
+        },
+        bindEventHub() {
+            window.eventHub.on('', (data) => {
+                console.log(data)
+            })
+        },
+    }
+    controller.init(view, model)
+}`)
+    fs.writeFileSync("./src/js/eventHub.js", 
+    `{
+    window.eventHub = {
+        events: {},
+        emit(eventName, data) {  // 发布
+            for(let key in this.events){
+                if(key === eventName){
+                    this.events[key].map(fn => fn.call(null, data))
+                }
+            }
+        },
+        on(eventName, cb) {  // 订阅
+            if(!this.events[eventName]){
+                this.events[eventName] =[]
+            }
+            this.events[eventName].push(cb)
+        }
+    }
 }`)
     fs.writeFileSync("README.md", "## "+dirName)
 
